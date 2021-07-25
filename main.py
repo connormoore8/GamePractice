f732f43adbc0e37148d19713c7618068c08fffc1
Connor Moore <connormoore8@gmail.com>
2021-07-25 17:43:43 -0500

    Beginning of text input.

--- main.py
+++ main.py
@@ -11,53 +11,58 @@ BLACK = (0, 0, 0)
 GREY = (160, 160, 160)
 WHITE = (255, 255, 255)
 dimension = 25
+
 # cellMap = np.random.randint(2, size=(10,10))
 # random state: np.random.randint(2, size=(15,15))
 # board[2] = [0,0,0,1,1,1,0,0,0,1,1,1,0,0,0]
 # board[6] = [0 for x in range(6)] + [1,1] + [0 for x in range(7)]
 # board[7] = [0 for x in range(6)] + [1,1] + [0 for x in range(7)]
 # [1 for x in range(board.shape[0])]
+text = ''
 # GLOBAL VARS, Using a Dictionary
 _VARS = {'surf': False, 'gridWH': 500, 'gridCells': dimension,
          'gridOrigin': (150, 150), 'lineWidth': 2,
-         'cellMap': np.zeros((dimension+3, dimension+3), dtype=int),
-         'state': False}
+         'cellMap': np.zeros((dimension+6, dimension+6), dtype=int),
+         'state': False, 'input': False,
+         'text': 'Base Dimension: 25, click on this box to adjust it', 'KeyBoardInput': False}
 
 
 def main():
     pygame.init()
+    font = pygame.font.Font(None, 32)
     delay = 10
     # Initial Setup
     _VARS['surf'] = pygame.display.set_mode(SCREENSIZE)
     # The loop proper, things inside this loop will
     # be called over and over until you exit the window
-    print(_VARS['cellMap'])
-    pygame.draw.rect(_VARS['surf'], 'black', (0+0, 0, 800, 100))
+    #print(_VARS['cellMap'])
+    _VARS['input'] = pygame.draw.rect(_VARS['surf'], 'black', (0+0, 0, 800, 100))
     while True:
         checkEvents()
-
         pygame.time.wait(delay)
+        txt_surface = font.render(_VARS['text'], True, 'white')
         _VARS['surf'].fill(GREY)
-        nextGen = np.zeros((_VARS['gridCells']+3, _VARS['gridCells']+3), dtype=int)
-        pygame.draw.rect(_VARS['surf'], 'black', (0, 0, 800, 100))
+        nextGen = np.zeros((_VARS['gridCells']+6, _VARS['gridCells']+6), dtype=int)
+        _VARS['input'] = pygame.draw.rect(_VARS['surf'], 'black', (0, 0, 800, 100))
         # draw a grid
         drawSquareGrid(_VARS['gridOrigin'], _VARS['gridWH'], _VARS['gridCells'])
         # place cells
         placeCells()
         # update entry
         if _VARS['state']:
-            delay = 250
+            delay = 500
             for x in range(_VARS['cellMap'].shape[1]):
                 for y in range(_VARS['cellMap'].shape[0]):
                     examineCell(x, y, nextGen)
             _VARS['cellMap'] = nextGen
             #print(_VARS['cellMap'])
+        _VARS['surf'].blit(txt_surface, (_VARS['input'].x + 5, _VARS['input'].y + 5))
         pygame.display.update()
 
 
 def examineCell(i, j, nextGen):
     # sum N Neighbors:
-    edge = _VARS['gridCells'] + 2
+    edge = _VARS['gridCells'] + 5
     if i == 0 or i == edge or j == 0 or j == edge:
         nextGen[j][i] = 0
         return
@@ -84,9 +89,8 @@ def placeCells():
     cellBorder = (_VARS['gridWH']/_VARS['gridCells'])*.15
     #cellBorder = 10
     celldimX = celldimY = (_VARS['gridWH']/_VARS['gridCells']) - (cellBorder*2)
-    for row in range(_VARS['cellMap'].shape[0]-3):
-        for column in range(_VARS['cellMap'].shape[1]-3):
-
+    for row in range(_VARS['cellMap'].shape[0]-6):
+        for column in range(_VARS['cellMap'].shape[1]-6):
             if _VARS['cellMap'][column+3][row+3] == 1:
                 col = WHITE
             # elif(cellMap[column][row]==2):
@@ -110,7 +114,12 @@ def checkEvents():
         if event.type == pygame.QUIT:
             sys.exit()
         # handle mouse events
-        elif event.type == pygame.MOUSEBUTTONUP:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if _VARS['input'].collidepoint(event.pos):
+                _VARS['KeyBoardInput'] = True
+            else:
+                _VARS['KeyBoardInput'] = False
+        if event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
             # print(pos)
             # convert to position on board.
@@ -119,8 +128,20 @@ def checkEvents():
             #original without buffer
             #_VARS['cellMap'][coord[0]][coord[1]] = 1
             _VARS['cellMap'][coord[0]+3][coord[1]+3] = 1
-        elif event.type == KEYDOWN and event.key == K_SPACE:
+        if event.type == KEYDOWN:
+            if _VARS['KeyBoardInput']:
+                if event.key == pygame.K_RETURN:
+                    print(_VARS['text'])
+                    _VARS['text'] = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    _VARS['text'] =_VARS['text'][:-1]
+                else:
+                    _VARS['text'] += event.unicode
+
+        if event.type == KEYDOWN and event.key == K_SPACE:
             _VARS['state'] = True
+
+
         elif event.type == KEYDOWN and event.key == K_q:
             pygame.quit()
             sys.exit()
